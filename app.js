cbf86e2b94ae9c79015a9570a1ac83881cfc044c
Ethan Williams <ethanwill253@gmail.com>
2022-10-24 16:01:45 -0500

    changing user auth from mongo encrypt to hashing function

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5")
 const ejs = require("ejs")
 const _ = require("lodash")
 
@@ -21,7 +21,6 @@ const userSchema = new mongoose.Schema({
 }, { versionKey: false })
 
 const secret = process.env.SECRET
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]})
 const User = new mongoose.model("User", userSchema)
 
 
@@ -45,7 +44,7 @@ app.post("/register", function(req, res) {
 
   const newUser = new User({
     email: email,
-    password: password
+    password: md5(password)
   })
 
   newUser.save(function(err) {
@@ -59,7 +58,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
   const email = req.body.username
-  const password = req.body.password
+  const password = md5(req.body.password)
 
   User.findOne({email: email}, function(err, foundUser) {
     if (err) {
